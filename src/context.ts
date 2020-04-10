17bee33dfd5da4ce7017d01c35d2c30305dc1060
Tristan <tristan@tristansouthwell.com>
2020-04-10 17:37:01 +1000

    refactor to allow for userId checking

--- src/context.ts
+++ src/context.ts
@@ -2,10 +2,12 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// TODO - Fix all the anys
 export interface Context {
   prisma: PrismaClient;
+  request: any;
 }
 
-export function createContext(): Context {
-  return { prisma };
+export function createContext(request: any): Context {
+  return { prisma, request };
 }
